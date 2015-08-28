18d72f5720aebd6ff48ad2e630a2c8b8e53c806d
Julien Tanay <julien.tanay@canal-plus.com>
2015-08-28 16:12:35 +0200

    Add config.json
    Configuration options are now stored in a separate file `config.json`.

--- index.js
+++ index.js
@@ -1,50 +1,43 @@
 var express = require('express');
 var morgan = require('morgan');
 
-
 var app = express();
-var compteur = 0;
-
 /* Paramètres du serveur */
 var routes = require('./routes.json');
-var staticRoot = '/var/www/html/public';
-// var portNumber = 80;
-var portNumber = 8080;
+var config = require('./config.json')
+var counter = 0;
+
 
 /* Définition des routes */
 /* La sélection (l'accueil) CSATALD */
-app.post('/GNCPF/vod/cplusod/ctx/json/g5r7t/csatod/xtc/ws/content/csatald_selection', function(req, res){
+app.post(config.apiRoot + 'csatod/xtc/ws/content/csatald_selection', function(req, res){
     res.setHeader('Content-Type', 'application/json');
-    if (compteur%2 == 0)
-        {
-        res.status(200).sendFile('/var/www/html/public/csatald_content/csatald_selection.json');
-        }
-    if (compteur%2 != 0)
-        {
-        res.status(200).sendFile('/var/www/html/public/csatald_content/csatald_selection2.json');
-        }
-    compteur++;
+    if (counter%2 == 0)
+        {res.status(200).sendFile(config.staticRoot + 'csatald_content/csatald_selection.json');}
+    if (counter%2 != 0)
+        {res.status(200).sendFile(config.staticRoot + 'csatald_content/csatald_selection2.json');}
+    counter++;
 });
-/* Explication variable compteur :
-ici, on utilise un compteur car à chaque chargement de CANALSAT A LA DEMANDE, pour une même route deux fichiers sont demandés;
-Le compteur permet donc de fournir ces fichiers au bon moment. De plus, on utilise un modulo dans la condition du if afin que cela 
-fonctionne dans le cas on recharge plusieurs fois CANALSAT A LA DEMANDE sans interruption du serveur. */
+/* Explication variable `counter` :
+Ici, on utilise un compteur car à chaque chargement de CANALSAT A LA DEMANDE,
+pour une même route deux fichiers sont demandés. Le compteur permet donc de
+fournir ces fichiers au bon moment. De plus, on utilise un modulo dans la
+condition du if afin que cela fonctionne dans le casoù on recharge plusieurs
+fois CANALSAT A LA DEMANDE sans interruption du serveur. */
 
 /* Le reste */
 for (var r in routes) {
     // console.log(r, "=>", routes[r])
-    app.post(r, function(req, res){
+    app.post(config.apiRoot + r, function(req, res){
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).sendFile(routes[r]);
+        res.status(200).sendFile(config.staticRoot + routes[r]);
     });
 };
 
 
-
-
 /* Configuring server ... */
 app.use(morgan('dev'));
-app.use(express.static(staticRoot));
+app.use(express.static(config.staticRoot));
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -68,5 +61,5 @@ app.use(function (req, res, next) {
 
 
 /* Starting server ... */
-app.listen(portNumber);
-console.log("Listening on", portNumber, "...")
+app.listen(config.portNumber);
+console.log("Listening on", config.portNumber, "...")
