e0103339b26e50f09a4169a90dc3b4778562de20
Jean-Thierry BONHOMME <jtbonhomme@gmail.com>
2015-10-05 19:10:03 +0200

    Removed key hack on start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 # PhotoBox STARTUP FILE
 
 # Hack : disable some keys...
-xmodmap -e "keycode 64 = 0x0000"
-xmodmap -e "keycode 107 = 0x0000"
-xmodmap -e "keycode 133 = 0x0000"
-xmodmap -e "keycode 134 = 0x0000"
+#xmodmap -e "keycode 64 = 0x0000"
+#xmodmap -e "keycode 107 = 0x0000"
+#xmodmap -e "keycode 133 = 0x0000"
+#xmodmap -e "keycode 134 = 0x0000"
 
 # Change current directory
 cd /home/photobox/r7PopUpStore
