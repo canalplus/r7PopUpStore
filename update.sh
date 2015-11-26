8e31e54ba9ffdd25328a292864f0b5688f31c042
Jean-Thierry BONHOMME <jtbonhomme@gmail.com>
2015-11-26 23:39:04 +0100

    Fixed update.sh

--- update.sh
+++ update.sh
@@ -1,23 +1,4 @@
-# Update script
-echo '###################################'
-echo '##### r7PopUpStore Update Script ##'
-
-# Checkout all the files
-echo 'Dismissing local changes...'
-git checkout -- *
-
-# Checkout master branch
-echo 'Checking out master branch...'
+#!/bin/bash
+git checkout -- .
 git checkout master
-
-# Pull the newest version
-echo 'Pulling latest version...'
 git pull
-
-# # Checkout all the files
-# echo 'Checking out pinned version...'
-# git checkout v1.1.0
-
-# Done !
-echo '##### r7PopUpStore is up to date. #'
-echo '###################################'
